0239755c3fc65a5f5f1a155feec31d9e10605108
wooplus <aunglinhtet992@gmail.com>
2023-02-19 16:53:18 -0500

    change photo

--- components/Info.jsx
+++ components/Info.jsx
@@ -4,7 +4,11 @@ import ReactDOM from "react-dom";
 export default function Info() {
   return (
     <div className="info-container">
-      <img src="../images/person.jpg" className="person-img" alt="" />
+      <img
+        src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        className="person-img"
+        alt=""
+      />
       <h1 className="info-name">Bryan Aung</h1>
       <h3 className="info-job">Web Developer</h3>
       <p className="info-website">bryan.aung.website</p>
